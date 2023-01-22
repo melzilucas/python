13daf5dd7b97a2e6ff63d48269a1c757c8368f18
melzilucas <melzi.lucas@outlook.com>
2023-01-22 20:13:31 -0300

    Add new class

---
+++ Aulas/aula51.py
@@ -0,0 +1,28 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+def multiplicar(*args):
+    total = 1
+    for numero in args:
+        total *= numero
+    return total
+
+resultado_funcao = multiplicar(10, 5)
+print(resultado_funcao)
+
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne se o número é par ou ímpar.
+
+def par_impar(numero):
+    resultado = numero % 2
+    if resultado == 0:
+        return f'o número {numero} é par'
+    return f'O número {numero} é impar' 
+       
+
+par_ou_impar = par_impar(15)
+print(par_ou_impar)
